02e076dbcdccad99a1a7a3cbb1ccf3cdf5b63087
Thirumoorthi28 <48052999+Thirumoorthi28@users.noreply.github.com>
2019-08-14 22:01:53 +0530

    Create countwords.java

---
+++ countwords.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+public class CountWords{
+    public static void main(String args[]){
+        String str;
+        int count=1;
+        Scanner s=new Scanner(System.in);
+        System.out.print("Enter The String : ");
+        str=s.nextLine();
+        for(int i=0;i<str.length();i++){
+            if(str.charAt(i)==' '){
+                count++;
+            }
+        }
+         System.out.println("The Number Of Words : "+count);
+    }
+}
