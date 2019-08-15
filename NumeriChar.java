8f76a1ab3b4857a2ff447a28a0abf76d2f5ba9e2
Thirumoorthi28 <48052999+Thirumoorthi28@users.noreply.github.com>
2019-08-15 22:53:38 +0530

    Create NumeriChar.java

---
+++ NumeriChar.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+public class NumericChar{
+    public static void main(String args[]){
+    int num=0;
+    Scanner s=new Scanner(System.in);
+    System.out.print("Enter The Strings Of : ");
+		String str=s.nextLine();
+		int digits=0;
+		for(int i=0;i<str.length();i++){
+		if(Character.isDigit(str.charAt(i))){
+        num++;
+		}
+		}
+		System.out.println("The Numeric Characters Of :"+num);
+	}
+}
